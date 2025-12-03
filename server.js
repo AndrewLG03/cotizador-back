3977171dbc898f0b762353fa8edcecc1ff23a4d5
AndrewLG03 <loaiza29andres@gmail.com>
2025-12-03 16:18:31 -0600

    Fix: No funciona front en render

--- server.js
+++ server.js
@@ -1,28 +1,16 @@
 // cotizador-back/server.js
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
-
 const app = express();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Rutas API
 app.use("/api/tipos", require("./routes/tipos.routes"));
 app.use("/api/materiales", require("./routes/materiales.routes"));
 app.use("/api/proyecto", require("./routes/proyecto.routes"));
 app.use("/api/cotizacion", require("./routes/cotizacion.routes"));
 
-// Servir frontend
-app.use(express.static(path.join(__dirname, "../front")));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../front/index.html"));
-});
-
-// Puerto
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
+app.listen(4000, () => {
+    console.log("Servidor corriendo en puerto 4000");
 });
